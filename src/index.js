f45c524f597c1171f1184e72592085d281d0bc78
Lucas-Melo0 <lucassss8v@gmail.com>
2022-09-12 07:47:32 -0300

    [chore] Add PORT constant on dotenv

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import usersRouter from "./routers/usersRouter.js";
 import transactionsRouter from "./routers/transactionsRouter.js";
 import { hasToken } from "./middlewares/authMiddleware.js";
 
 const server = express();
+dotenv.config();
 server.use(express.json());
 server.use(cors());
 server.use(usersRouter);
 server.use(hasToken);
 server.use(transactionsRouter);
 
-server.listen(5000, () => {
+server.listen(process.env.PORT, () => {
   console.log("listen on 5000");
 });
